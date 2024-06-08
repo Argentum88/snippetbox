5c63afa0d430c529e22106dc1615015db7ffcfcc
argentum88 <argentimgipan@gmail.com>
2024-06-08 21:21:22 +0500

    single-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/Argentum88/snippetbox/internal/models"
 	"net/http"
 	"strconv"
 	"text/template"
@@ -37,7 +39,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serveError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
